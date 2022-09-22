10c2a581e768081705cf99343f2c68ea2fdc813f
vipin kumar <vipinoo7@yahoo.in>
2022-09-22 07:39:21 +0530

    completed getEventsWithWorkshops

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    /**
+     * Get the workshops for the blog post.
+     */
+    public function workshops()
+    {
+        return $this->hasMany(Workshop::class);
+    }
 }
--- app/Models/Workshop.php
+++ app/Models/Workshop.php
@@ -5,7 +5,6 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Date;
 
 class Workshop extends Model
 {
